c8e466132d303e7ebe2f7cf60b7fabfa37fe9269
BryanFrank82 <148111531+BryanFrank82@users.noreply.github.com>
2025-04-16 15:59:23 -0700

    Update Cup.java

--- Cup.java
+++ Cup.java
@@ -7,29 +7,41 @@ public class Cup{
 	private ArrayList<Dice>outCup;
 	
 	public Cup(){
+		inCup = new ArrayList<Dice>();
 		for(int i = 0;i<5;i++) {
 			Dice dice = new Dice();
 			inCup.add(dice);
 		}
+		outCup = new ArrayList<Dice>();
 	}
 	
 	public void addDiceToCup(int pos) {
-		if(pos <= outCup.size()) {
+		if(outCup.size() > 0) {
 			Dice dice = outCup.remove(pos);
 			inCup.add(dice);
 		}
 	}
 	
 	public void removeDiceFromCup(int pos) {
-		if(pos <= inCup.size()) {
+		if(inCup.size() > 0) {
 			Dice dice = inCup.remove(pos);
 			outCup.add(dice);
 		}
 	}
 	
-	public void rollDicex() {
+	public void rollDice() {
 		for(int i = 0;i<inCup.size();i++) {
 			inCup.get(i).roll();
 		}
+	}
+	
+	public ArrayList<Dice> getInDice() {
+		ArrayList<Dice> inCupCopy = inCup;
+		return inCupCopy;
+	}
+	
+	public ArrayList<Dice> getOutDice() {
+		ArrayList<Dice> outCupCopy = outCup;
+		return outCupCopy;
 	}
 }
