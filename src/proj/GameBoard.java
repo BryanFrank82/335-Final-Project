43b77424b82012826f056c2498f8974052939123
MuyangChen9 <muyangchen@arizona.edu>
2025-04-19 08:56:58 -0700

    Update GameBoard.java

--- src/proj/GameBoard.java
+++ src/proj/GameBoard.java
@@ -1,20 +1,29 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 
 
 public class GameBoard {
 	private List<Player> players;
 	private int currentIndex;
+	private final PlayerLibrary library;
+
 	
 	
 	
-	 public GameBoard() {
+	 public GameBoard(PlayerLibrary library) {
 	        this.players = new ArrayList<>();
 	        this.currentIndex = 0;
+	        this.library = library;
 	    }
 	 
-	 public void addPlayer(Player player) {
-	        players.add(player);
+	 public boolean addPlayer(Player player) {
+	        if (library.containsPlayer(player)) {
+	            players.add(player);
+	            return true;
+	        }
+	        return false;
 	    }
 	 
 	 
@@ -41,6 +50,36 @@ public class GameBoard {
 			 return true;
 		 }
 		 return false;
+	 }
+	 
+	 
+	 public void getOrder() {
+		 List<Player> humans = new ArrayList<>();
+	     List<Player> computers = new ArrayList<>();
+	     
+	     for (Player p : players) {
+	            if (p.getType() == Player.PlayerType.HUMAN) {
+	                humans.add(p);
+	            } else {
+	                computers.add(p);
+	            }
+	        }
+	     
+	     Map<Player, Integer> rollResults = new HashMap<>();
+	     for (Player h : humans) {
+	    	 Cup cup = new Cup();
+	            cup.rollDice();
+	            int sum = 0;
+	            for (Dice d : cup.getInDice()) {
+	            	sum += d.getCurrentValue().ordinal() + 1;
+	            }
+	            rollResults.put(h, sum);
+	     }
+	     humans.sort((a, b) -> rollResults.get(b) - rollResults.get(a));
+	     players.clear();
+	     players.addAll(humans);
+	     players.addAll(computers);
+		 
 	 }
 
 }
